4fdb8775c2e3a66a5d4bd574550d803030f085be
Mekonen-Abera <makoninabera@gmail.com>
2023-12-14 23:08:08 -0800

    C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,24 +21,24 @@ void swap_int(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-    int has_swapped = 1; // Flag to check if any swaps were made
+    int has_swapped = 1;
     size_t i, end = size;
 
     // looping until no swaps are made in a pass
     while (has_swapped == 1)
     {
-        has_swapped = 0; // Reset the swap flag
+        has_swapped = 0;
         for (i = 1; i < end; i++)
         {
             // If the current element is greater than the next one, swap them
             if (array[i - 1] > array[i])
             {
                 swap_int(&array[i - 1], &array[i]);
-                has_swapped = 1; // Set the swap flag
-                print_array(array, size); // Print the array after each swap
+                has_swapped = 1;
+                print_array(array, size);
             }
         }
-        end--; // Reduce the end boundary as the largest element is now at the end
+        end--;
     }
 }
 

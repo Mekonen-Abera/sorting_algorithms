2eea870bbc9f28649ab663ad8b153facdc5f95c2
Mekonen-Abera <makoninabera@gmail.com>
2023-12-15 04:19:31 -0800

    Buble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,61 +3,42 @@
 
 /**
  * swap_int - Swaps the values of two integers.
- * @a: Pointer to the first integer.
- * @b: Pointer to the second integer
- * Done by: Mekonen-Abera
+ * @a: Pointer to integer a.
+ * @b: Pointer to integer b.
  */
 void swap_int(int *a, int *b)
 {
-    int temp;
+	int temp;
 
-    temp = *a;
-    *a = *b;
-    *b = temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
- * print_array - Prints the elements of an array.
- * @array: An integer pointer that points to an array of integers.
- * @size: The number of components in the array.
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    for (i = 0; i < size; i++)
-    {
-        printf("%d", array[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
-}
-
-/**
- * bubble_sort - Sorts an array of integers in increasing order
- *               using the Bubble Sort algorithm.
- * @array: Array of integers pointer.
- * @size: Number of elements in the array.
+ * bubble_sort - Sorts an array of integers in ascending order
+ *               
+ * @array: Pointer to an array of integers.
+ * @size: The number of elements in the array.
  */
 void bubble_sort(int *array, size_t size)
 {
-    int needs_swap = 1;
-    size_t i, pass_length = size;
+	int swapped = 1;
+	size_t i, length = size;
 
-    while (needs_swap == 1)
-    {
-        needs_swap = 0;
-        for (i = 1; i < pass_length; i++)
-        {
-            if (array[i - 1] > array[i])
-            {
-                swap_int(&array[i - 1], &array[i]);
-                needs_swap = 1;
-                print_array(array, size);
-            }
-        }
-        pass_length--;
-    }
+	while (swapped == 1)
+	{
+		swapped = 0;
+		for (i = 1; i < length; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				swap_int(&array[i - 1], &array[i]);
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		length--;
+	}
 }
 

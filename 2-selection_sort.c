124ae3d684817eb1cdd4e0da1fc16e2b5d6d511b
Mekonen-Abera <makoninabera@gmail.com>
2023-12-15 04:55:46 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -43,7 +43,7 @@ void selection_sort(int *array, size_t size)
 		if (array + i != min)
 		{
 			swap_ints(array + i, min);
-			putchar(print_array(array, size));
+			print_array(array, size);
 		}
 	}
 }

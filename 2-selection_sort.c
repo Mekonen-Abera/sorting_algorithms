2dcd76baf6047c89177f15f0c561e31fa45a76a1
Mekonen-Abera <makoninabera@gmail.com>
2023-12-15 05:13:57 -0800

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,42 +1,45 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * swap_ints - Exchanges the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
  */
 void swap_ints(int *a, int *b)
 {
-	int tmp;
+	int temp;
 
-	tmp = *a;
+	temp = *a;
 	*a = *b;
-	*b = tmp;
+	*b = temp;
 }
 
 /**
- * selection_sort - Sort an array of integers in ascending order
- *                  using the selection sort algorithm.
+ * selection_sort - Sorts an array of integers in increasing order
  * @array: An array of integers.
  * @size: The size of the array.
- *
- * Description: Prints the array after each swap.
+ * By: Mekonen & Gebrekidan
+ * Description: Prints the array following a swap in every event.
  */
 void selection_sort(int *array, size_t size)
 {
 	int *min;
 	size_t i, j;
 
+	/* Check if array is NULL or size is less than 2 */
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min = array + i;
+
+		/* Locate the index of the smallest element among the unsorted entries */
 		for (j = i + 1; j < size; j++)
 			min = (array[j] < *min) ? (array + j) : min;
 
-		if ((array + i) != min)
+		/* Swap the found minimum value with the first element */
+		if (array + i != min)
 		{
 			swap_ints(array + i, min);
 			print_array(array, size);

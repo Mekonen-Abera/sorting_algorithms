dd0d0f3965c2349d762b5298cf4b236a6a92a30e
Mekonen-Abera <makoninabera@gmail.com>
2023-12-15 03:14:24 -0800

    Insersion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,55 +1,63 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- * in ascending order using the Insertion Sort algorithm.
- * @list: Double pointer to the double linked list.
+ * swap_nodes - Swaps two nodes in a doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to be swaped.
+ * @n2: The second node to be swapped.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	/* Adjust next pointers for the first node */
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+
+	/* Adjust prev pointers for the second node */
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+
+	/* Update next pointer for the previous node of the first node */
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		/* If the first node was the head, update the head */
+		*h = n2;
+
+	/* Update prev pointer for the first node */
+	(*n1)->prev = n2;
+	/* Update n1 to be the previous node of the second node */
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integersi
+ * @list: A pointer to the head of a doubly-linked list of integers.
  * Done by: Mekonen Abera
+ * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *div1 = NULL, *aux = NULL, *tmp = NULL;
+	listint_t *iter, *insert, *tmp;
 
-	/* Check if the list and *list are valid */
-	if (!list || !*list)
+	/* Check for a valid list with more than one element */
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	/* Check if there is more than one element in the list */
-	if ((*list)->next)
+	/* Iterate through the list starting from the second element */
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		div1 = *list;
-		div1 = div1->next;
+		/* Save the next node before potential swaps */
+		tmp = iter->next;
+		/* Start the insert pointer from the previous node */
+		insert = iter->prev;
 
-		/* Iterate through the list starting from the second element */
-		while (div1)
+		/* Swap nodes until in the correct order */
+		while (insert != NULL && iter->n < insert->n)
 		{
-			/* Save the current node in auxiliary variable */
-			aux = div1;
-			div1 = div1->next;
-
-			/* Move backward and swap nodes until in the correct order */
-			while (aux->prev && aux->prev->n > aux->n)
-			{
-				/* Save the previous node */
-				tmp = aux->prev;
-				/* Adjust the pointers for the swap */
-				aux->prev = tmp->prev;
-				if (tmp->prev)
-					tmp->prev->next = aux;
-				tmp->next = aux->next;
-				if (tmp->next)
-					tmp->next->prev = tmp;
-				tmp->prev = aux;
-				aux->next = tmp;
-
-				/* If the node is moved to the beginning, update the list */
-				if (!aux->prev)
-					*list = aux;
-
-				/* Update the list for printing and continue swapping */
-				tmp = *list;
-				print_list(tmp);
-			}
+			/* Swap nodes and print the list after each swap */
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
 	}
 }

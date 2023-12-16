786d10def01e75d0dfc4cd718caa388133b778e9
Mekonen-Abera <makoninabera@gmail.com>
2023-12-16 11:41:19 -0800

    Sorting Algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,46 @@
 #include "sort.h"
 
 /**
-* bubble_sort - sorts an array of integers in ascending order using
-* Bubble Sort algorithm
-* @array: array to be sorted
-* @size: size of the array
-*/
+ * swap_two_int - Swaping two integers in an array.
+ * @x: The first integer to swap.
+ * @y: The second integer to swap.
+ */
+void swap_two_int(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t temp, i, j;
+	size_t x, leng = size;
+	bool bub = false;
 
-	if (size <= 1)
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)/*iterates throught entire array*/
-	{/*iterates & compares elements with the next up to the final element*/
-		for (j = 0; j < size - 1; j++)
+	while (bub == false)
+	{
+		bub = true;
+		for (x = 0; x < leng - 1; x++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[x] > array[x + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swap_two_int(array + x, array + x + 1);
 				print_array(array, size);
+				bub = false;
 			}
 		}
+		leng--;
 	}
 }

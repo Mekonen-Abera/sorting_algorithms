d633463817b9dea95013ccba8353235c95ca1891
Mekonen-Abera <makoninabera@gmail.com>
2023-12-16 13:28:19 -0800

    Sorting Algorithms

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,13 +1,13 @@
 #include "sort.h"
 
 /**
- * sift_down - Performs the sift-down operation to maintain the heap property
+ * swiftdown - Adjusts the heap by swapping elements down the tree.
  * @array: Pointer to the array.
  * @size_init: Original size of the array.
- * @size: Current size of the array.
- * @i: Index of the root of the tree.
+ * @size: Current size of the heap.
+ * @i: Index representing the root of the tree.
  */
-void sift_down(int *array, size_t size_init, size_t size, size_t i)
+void swiftdown(int *array, size_t size_init, size_t size, size_t i)
 {
 	int n, branch1, branch2;
 	size_t br1, br2;
@@ -15,40 +15,36 @@ void sift_down(int *array, size_t size_init, size_t size, size_t i)
 	br1 = i * 2 + 1;
 	br2 = br1 + 1;
 
-	/* Check if both branches are within the current size of the array */
-	if ((br1 < size) && (br2 < size))
-	{
-		branch1 = array[br1];
-		branch2 = array[br2];
+	/* Determine values of branches */
+	branch1 = (br1 < size) ? array[br1] : 0;
+	branch2 = (br2 < size) ? array[br2] : 0;
 
-		/* Check which branch is greater and update the root if needed */
-		if ((branch1 >= branch2 && branch1 > array[i]) ||
-			((br1 == size - 1) && branch1 > array[i]))
-		{
-			n = array[i];
-			array[i] = branch1;
-			array[br1] = n;
-			print_array(array, size_init);
-		}
-		else if ((br2 < size) && (branch2 > branch1 && branch2 > array[i]))
-		{
-			n = array[i];
-			array[i] = branch2;
-			array[br2] = n;
-			print_array(array, size_init);
-		}
+	/* Compare and swap if necessary */
+	if ((branch1 >= branch2 && branch1 > array[i]) ||
+	    (br1 == size - 1 && branch1 > array[i]))
+	{
+		n = array[i];
+		array[i] = branch1;
+		array[br1] = n;
+		print_array(array, size_init);
+	}
+	else if (br2 < size && branch2 > branch1 && branch2 > array[i])
+	{
+		n = array[i];
+		array[i] = branch2;
+		array[br2] = n;
+		print_array(array, size_init);
 	}
 
-	/* Recursively apply sift-down to the left and right branches */
+	/* Recursively check left and right subtrees */
 	if (br1 < size - 1)
-		sift_down(array, size_init, size, br1);
+		swiftdown(array, size_init, size, br1);
 	if (br2 < size - 1)
-		sift_down(array, size_init, size, br2);
+		swiftdown(array, size_init, size, br2);
 }
 
 /**
  * heap_sort - Sorts an array of integers in ascending order
- * using the Heap sort algorithm.
  * @array: Pointer to the array.
  * @size: Size of the array.
  */
@@ -58,23 +54,22 @@ void heap_sort(int *array, size_t size)
 	int n;
 
 	/* Check if the array is valid for sorting */
-	if (!array)
+	if (!array || size < 2)
 		return;
 
-	/* Build a max heap using sift-down */
-	for (i = 0; i < size / 2; i++)
-	{
-		sift_down(array, size_init, size, size / 2 - 1 - i);
-	}
+	/* Build the max heap */
+	for (i = size / 2; i > 0; i--)
+		swiftdown(array, size_init, size, i - 1);
 
-	/* Extract elements from the heap and perform sift-down */
+	/* Heap sort */
 	for (i = 0; i < size_init - 1; i++)
 	{
+		/* Swap the root with the last element and adjust the heap */
 		n = array[0];
 		array[0] = array[size - 1 - i];
 		array[size - 1 - i] = n;
 		print_array(array, size_init);
-		sift_down(array, size_init, size - i - 1, 0);
+		swiftdown(array, size_init, size - i - 1, 0);
 	}
 }
 

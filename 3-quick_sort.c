2183b9e6925330717aebf3f306b187f6578c6ac3
Mekonen-Abera <makoninabera@gmail.com>
2023-12-19 00:19:43 -0800

    Quick sorting

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,35 +1,35 @@
 #include "sort.h"
 
 /**
- * swap - swap two items
- * @a: Int
- * @b: Int
+ * swap - Swaps two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ * Done by: Mekonen & Gebrekidan
  */
-
 void swap(int *a, int *b)
 {
-	int aux;
+	int temp;
 
-	aux = *a;
+	temp = *a;
 	*a = *b;
-	*b = aux;
+	*b = temp;
 }
 
 /**
- * partition - exceutes the partition process
- * @array: pointer to array to sort
- * @low: index of the low element
- * @high: index of the high element
- * @size: size of array for partitioning
- * Return: index o the partition
+ * partition - Carries out the partition procedure of Quick Sort.
+ * @array: Array to Sort.
+ * @low: Index of the low element.
+ * @high: Index of the high element.
+ * @size: The size of the array for partitioning.
+ *
+ * Return: Index of the partition.
  */
-
 int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = (low - 1), j;
+	int i = low - 1, j;
 
-	for (j = low; j <= high - 1; j++)
+	for (j = low; j <= high - 1; ++j)
 	{
 		if (array[j] <= pivot)
 		{
@@ -39,19 +39,20 @@ int partition(int *array, int low, int high, size_t size)
 				print_array(array, size);
 		}
 	}
+
 	swap(&array[i + 1], &array[high]);
 	if (i + 1 != high)
 		print_array(array, size);
+
 	return (i + 1);
 }
 
 /**
- * quick_sort2 - sorts an array of integers in ascending order
- * using Quick Sort algorithm
- * @array: pointer to array to sort
- * @low: index of the low element
- * @high: index of the high element
- * @size: size of array to sort
+ * quick_sort2 - Sorts an array of integers in ascending order
+ * @array: The array to sort.
+ * @low: Index of the low element.
+ * @high: Index of the high element.
+ * @size: The size of the array to be sorted.
  */
 void quick_sort2(int *array, int low, int high, size_t size)
 {
@@ -64,13 +65,12 @@ void quick_sort2(int *array, int low, int high, size_t size)
 		quick_sort2(array, pivot + 1, high, size);
 	}
 }
-/**
- * quick_sort - sorts an array of integers in ascending
- * order using the Quick sort algorithm
- * @array: pointer to array to sort
- * @size: size of array to sort
- */
 
+/**
+ * quick_sort - Arranges an array of integers in ascending order
+ * @array: Array to be sorted.
+ * @size: Size of the array to be sorted.
+ */
 void quick_sort(int *array, size_t size)
 {
 	int high = size - 1;
@@ -78,3 +78,4 @@ void quick_sort(int *array, size_t size)
 	if (size > 1)
 		quick_sort2(array, 0, high, size);
 }
+

835986a344392bdaf0fc74e8c105d2d82b0acd33
Mekonen-Abera <makoninabera@gmail.com>
2023-12-19 00:28:26 -0800

    Shell sorting

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,29 +1,33 @@
 #include "sort.h"
+
 /**
- * shell_sort - sorts an array of integers in ascending order
- * using the Shell sort algorithm, using the Knuth sequence.
- * @array: Pointer to array for sorting
- * @size: size of array to sort
+ * shell_sort - Sorts an array of integers in ascending order using Shell sort
+ *               algorithm with the Knuth sequence.
+ * @array: The array to be sorted.
+ * @size: Size of the array.
  */
 void shell_sort(int *array, size_t size)
 {
 	size_t gap = 1, i, cmp;
 	int temp;
 
-/*validations*/
+	/* Check if the array is empty or contains only one element */
 	if (size == 0 || size == 1)
 		return;
-/*Finds the max gap*/
-	while (gap < (size / 3))
-		gap = gap * 3 + 1;
 
-/*comparing values from begining and from gap*/
+	/* Calculate the maximum gap in the Knuth sequence */
+	while (gap < (size / 3))
+		gap = (gap * 3) + 1;
+
+	/* Compare elements from the beginning and with the gap */
 	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
 			cmp = i;
+
+			/* Perform insertion sort within the current gap */
 			while (cmp >= gap && array[cmp - gap] > temp)
 			{
 				array[cmp] = array[cmp - gap];
@@ -31,8 +35,11 @@ void shell_sort(int *array, size_t size)
 				array[cmp] = temp;
 			}
 		}
+
+		/* Print the current state of the array after each gap */
 		print_array(array, size);
-/*Gap decreases according to Knuth sequence*/
+
+		/* Update the gap using the Knuth sequence */
 		gap /= 3;
 	}
 }

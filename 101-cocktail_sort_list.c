ad09f9c606e86bae4da6cc5bd27868ecc05fcfa9
Mekonen-Abera <makoninabera@gmail.com>
2023-12-19 00:45:57 -0800

    Cocktail sorting

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,55 +1,64 @@
 #include "sort.h"
+
 /**
- * swap1 - swaps nodes from left to right
- * @list: pointer to list
- * @head: pointer to head node
- * @aux: auxiliar pointer
- * Return: no return
+ * swap_left_to_right - Swaps nodes from left to right in a doubly linked list.
+ * @list: Pointer to the list.
+ * @head: Pointer to the head node.
+ * @aux: Auxiliary pointer.
  */
-void swap1(listint_t **list, listint_t *head, listint_t *aux)
+void swap_left_to_right(listint_t **list, listint_t *head, listint_t *aux)
 {
+	/* Adjust previous and next pointers. */
 	if (head->prev)
 		head->prev->next = aux;
 	else
 		*list = aux;
 	if (aux->next)
 		aux->next->prev = head;
+
+	/* Update node connections */
 	head->next = aux->next;
 	aux->prev = head->prev;
 	aux->next = head;
 	head->prev = aux;
-	print_list(*list);
 
+	/* Print the current state of the list */
+	print_list(*list);
 }
+
 /**
- * swap2 - swaps nodes from right to left
- * @list: pointer to list
- * @head: pointer to head node
- * @aux: auxiliar pointer
- * Return: no return
+ * swap_right_to_left - Swaps nodes from right to left in a doubly linked list.
+ * @list: Pointer to the list.
+ * @head: Pointer to the head node.
+ * @aux: Auxiliary pointer.
  */
-void swap2(listint_t **list, listint_t *head, listint_t *aux)
+void swap_right_to_left(listint_t **list, listint_t *head, listint_t *aux)
 {
 	aux = head->prev;
+
+	/* Update previous and next pointers */
 	aux->next->prev = aux->prev;
 	if (aux->prev)
 		aux->prev->next = aux->next;
 	else
 		*list = aux->next;
+
+	/* Update node connections */
 	aux->prev = aux->next;
 	aux->next = aux->next->next;
 	aux->prev->next = aux;
 	if (aux->next)
 		aux->next->prev = aux;
+
+	/* Print the current state of the list */
 	print_list(*list);
 }
 
 /**
- * cocktail_sort_list - sorts a doubly linked list of integers
- * in ascending order using the Cocktail sort ailgorithm
- * @list: pointer to the list head
- * Return: no return
- **/
+ * cocktail_sort_list - Sorts a doubly linked list of integers
+ *                      in ascending order by the Cocktail sort algorithm.
+ * @list: Pointer to the list head.
+ */
 void cocktail_sort_list(listint_t **list)
 {
 	listint_t *head, *aux;
@@ -61,31 +70,37 @@ void cocktail_sort_list(listint_t **list)
 		while (flag != 0)
 		{
 			flag = 0;
+
+			/* Traverse the list from left to right. */
 			while (head->next)
 			{
 				if (head->n > head->next->n)
 				{
 					aux = head->next;
-					swap1(list, head, aux);
+					swap_left_to_right(list, head, aux);
 					flag = 1;
 				}
 				else
 					head = head->next;
 			}
+
+			/* In case there were no swaps, break out of the iteration. */
 			if (flag == 0)
 				break;
+
 			flag = 0;
+
+			/* Go through the list starting from its end and move towards its beginning. */
 			while (head->prev)
 			{
 				if (head->prev->n > head->n)
 				{
-					swap2(list, head, aux);
+					swap_right_to_left(list, head, aux);
 					flag = 1;
 				}
 				else
 					head = head->prev;
 			}
-
 		}
 	}
 }
